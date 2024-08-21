30024de0cb49205c125af9459be073463292d636
francescodela <franklin.hernandez@correounivalle.edu.co>
2024-08-21 10:14:17 -0500

    Add files via upload

---
+++ franklinlaboratorio1.py
@@ -0,0 +1,12 @@
+"""
+taller 1 de fundamentos de programacion / franklin hernandez 
+
+"""
+class Auto :
+    marca = "la marca es: ford"
+    año= "el año del vehicul es : 2024 "
+    placa = "la placa del vehiculo es : f3h4jk"
+taxi = Auto() 
+print (taxi.marca )
+print (taxi.año)
+print (taxi.placa )
